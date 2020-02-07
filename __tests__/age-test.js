ede76b52eae6777f95cdf6b23f439b4d3a55b274
Michelle Morin <morin2m@gmail.com>
2020-02-07 11:00:32 -0800

    remove userbiology function call from all tests that previously called userbiology, fail tests

--- __tests__/age-test.js
+++ __tests__/age-test.js
@@ -31,39 +31,39 @@ describe('Age', () => {
   });
 
   test('should correctly update user biological information', () => {
-    userAge.userBiology("F");
+    // userAge.userBiology("F");
     expect(userAge.biology).toBe("F");
   });
 
   test('should determine user life expectancy on Earth based on user biology property', () => {
-    userAge.userBiology("F");
+    // userAge.userBiology("F");
     userAge.earthExpectancy();
     expect(userAge.earthLifeExpectancy).toBe(81);
   });
 
   test('should determine number of years a user has left to live on Mercury', () => {
-    userAge.userBiology("F");
+    // userAge.userBiology("F");
     userAge.earthExpectancy();
     userAge.mercuryExpectancy();
     expect(userAge.yearsLeftMercury).toBe(212.50);
   });
 
   test('should determine number of years a user has left to live on Venus', () => {
-    userAge.userBiology("F");
+    // userAge.userBiology("F");
     userAge.earthExpectancy();
     userAge.venusExpectancy();
     expect(userAge.yearsLeftVenus).toBe(82.26);
   });
 
   test('should determine number of years a user has left to live on Mars', () => {
-    userAge.userBiology("F");
+    // userAge.userBiology("F");
     userAge.earthExpectancy();
     userAge.marsExpectancy();
     expect(userAge.yearsLeftMars).toBe(27.13);
   });
 
   test('should determine number of years a user has left to live on Jupiter', () => {
-    userAge.userBiology("F");
+    // userAge.userBiology("F");
     userAge.earthExpectancy();
     userAge.jupiterExpectancy();
     expect(userAge.yearsLeftJupiter).toBe(4.30);
@@ -71,7 +71,7 @@ describe('Age', () => {
 
   test('should determine number of Mercury years a user has lived beyond life expectancy', () => {
     let newUserAge = new Age(85);
-    newUserAge.userBiology("F");
+    // newUserAge.userBiology("F");
     newUserAge.earthExpectancy();
     newUserAge.determineBeyondExpectancy();
     expect(newUserAge.mercuryYearsBeyondExpectancy).toBe(16.67);
@@ -79,7 +79,7 @@ describe('Age', () => {
 
   test('should determine number of Venus years a user has lived beyond life expectancy', () => {
     let newUserAge = new Age(85);
-    newUserAge.userBiology("F");
+    // newUserAge.userBiology("F");
     newUserAge.earthExpectancy();
     newUserAge.determineBeyondExpectancy();
     expect(newUserAge.venusYearsBeyondExpectancy).toBe(6.45);
@@ -87,7 +87,7 @@ describe('Age', () => {
 
   test('should determine number of Mars years a user has lived beyond life expectancy', () => {
     let newUserAge = new Age(85);
-    newUserAge.userBiology("F");
+    // newUserAge.userBiology("F");
     newUserAge.earthExpectancy();
     newUserAge.determineBeyondExpectancy();
     expect(newUserAge.marsYearsBeyondExpectancy).toBe(2.13);
@@ -95,7 +95,7 @@ describe('Age', () => {
 
   test('should determine number of Jupiter years a user has lived beyond life expectancy', () => {
     let newUserAge = new Age(85);
-    newUserAge.userBiology("F");
+    // newUserAge.userBiology("F");
     newUserAge.earthExpectancy();
     newUserAge.determineBeyondExpectancy();
     expect(newUserAge.jupiterYearsBeyondExpectancy).toBe(0.34);
