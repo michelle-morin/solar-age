472c6b055c1ae589317dfb5a5700d8ba184a66e1
Michelle Morin <morin2m@gmail.com>
2020-02-07 09:09:55 -0800

    add business logic for age constructor and pass age object creation test

--- src/age.js
+++ src/age.js
@@ -1,5 +1,17 @@
 export class Age {
   constructor(earthAge) {
-
+    this.earthAge = earthAge;
+    this.mercuryAge = 0;
+    this.venusAge = 0;
+    this.marsAge = 0;
+    this.jupiterAge = 0;
+    this.biology = "M";
+    this.earthLifeExpectancy = 76;
+    this.yearsLeftMercury = 0;
+    this.yearsLeftVenus = 0;
+    this.yearsLeftMars = 0;
+    this.yearsLeftJupiter = 0;
+    this.yearsBeyondExpectancy = 0;
   }
+  
 }
