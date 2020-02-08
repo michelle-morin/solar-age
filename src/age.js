6f579161da26861c08a6bfd66b5a8435806c27ce
Michelle Morin <morin2m@gmail.com>
2020-02-07 16:24:42 -0800

    remove equal to clause from determinePlanetExpectancies function

--- src/age.js
+++ src/age.js
@@ -35,7 +35,7 @@ export class Age {
   }
 
   determinePlanetExpectancies() {
-    if (this.earthAge <= this.earthLifeExpectancy) {
+    if (this.earthAge < this.earthLifeExpectancy) {
       this.yearsLeftMercury = parseFloat(((this.earthLifeExpectancy - this.earthAge) / 0.24).toFixed(2));
       this.yearsLeftVenus = parseFloat(((this.earthLifeExpectancy - this.earthAge) / 0.62).toFixed(2));
       this.yearsLeftMars = parseFloat(((this.earthLifeExpectancy - this.earthAge) / 1.88).toFixed(2));
